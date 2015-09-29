1447bf92c37a9f9c8e7e564abfbcc46db445ed98
unknown <sdeshpande@LTSDESHPANDE1.blinkx.local>
2015-09-29 12:36:01 +0100

    more experiments VOGELLA.com

--- src/FileIO/FileExperiments.java
+++ src/FileIO/FileExperiments.java
@@ -38,4 +38,18 @@ public class FileExperiments {
                 .forEach(System.out::println);
     }
 
+    public static String readTextFile(String fileName) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(fileName)));
+        return content;
+    }
+
+    public static List<String> readTextFileByLines(String fileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(fileName));
+        return lines;
+    }
+
+    public static void writeToTextFile(String fileName, String content) throws IOException {
+        Files.write(Paths.get(fileName), content.getBytes(), StandardOpenOption.CREATE);
+    }
+
 }
